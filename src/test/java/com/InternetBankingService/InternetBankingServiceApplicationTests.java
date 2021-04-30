d708467b1cd88c1a05747cceb0c05ec3064e9173
Bigsam11 <jrmhaustin@gmail.com>
2021-05-01 06:01:31 +0800

    Initializing new solution

---
+++ src/test/java/com/InternetBankingService/InternetBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InternetBankingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
